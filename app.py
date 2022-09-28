fe67212cb8ca78a0fd31371f45a83915c00b26cf
milanochuang <milanochuang@gmail.com>
2022-09-28 22:19:35 +0800

    Change slightly

--- app.py
+++ app.py
@@ -10,4 +10,8 @@ st.image("dora.png")
 test_str = st.text_input('請在這裡打上任意文字')
 if test_str:
     result = SnowNLP(test_str)
-    st.markdown('你的分數是 **{}**'.format(result.sentiments))
+    st.markdown('你的分數是**{}** 分'.format(result.sentiments))
+    if result.sentiments <= 5:
+        st.markdown('*生活要過的放鬆一點喔*')
+    else:
+        st.markdown('*很好很好，繼續保持正向喔！*')
